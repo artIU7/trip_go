ff779ff4aea2682602cb1d07f7ebf024fff72c14
brazilec22 <stratienko.artem@gmail.com>
2021-10-23 14:41:16 +0300

    auto commit - trip_go

---
+++ client/trip_creater/trip_creater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trip_creater
+//
+//  Created by Артем Стратиенко on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
